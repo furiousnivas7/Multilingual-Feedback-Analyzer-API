05948ebd981df61b77a1877d86e126e65c8df687
Nivash_sharma <nivasmathu@gmail.com>
2026-05-05 01:35:59 +0530

    change componets

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 import { errorHandler } from './middlewares/errorHandler';
 import authRoutes from './routes/authRoutes';
@@ -7,6 +8,10 @@ import analysisRoutes from './routes/analysisRoutes';
 
 const app = express();
 
+app.use(cors({
+  origin: ['http://localhost:3001', 'http://localhost:3000'],
+  credentials: true,
+}));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
